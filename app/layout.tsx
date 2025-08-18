67b92ade99b213bb546d4bfe13d014138619e9b6
Teiming <36464297+Teiming@users.noreply.github.com>
2025-08-19 00:27:11 +0900

    feat: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,21 @@
+import { Metadata, Viewport } from 'next'
 import { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: 'After Sunday Project',
+  other: {
+    charSet: 'utf-8',
+  },
+}
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover',
+}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang='ko'>
       <body>{children}</body>
     </html>
   )
