79e92fe50714c900465965c2c3913d478556a6f6
Teiming <36464297+Teiming@users.noreply.github.com>
2025-08-30 23:15:31 +0900

    design

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export const metadata: Metadata = {
   },
 }
 export const viewport: Viewport = {
+  themeColor: 'deepskyblue',
   width: 'device-width',
   initialScale: 1,
   viewportFit: 'cover',
