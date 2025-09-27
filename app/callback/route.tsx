8afdce2d674da43eb1e4f263cf513ce7ea401887
Teiming <36464297+Teiming@users.noreply.github.com>
2025-09-28 00:23:32 +0900

    feat: Interface KakaoToken

--- app/callback/route.tsx
+++ app/callback/route.tsx
@@ -1,6 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server'
 import jwt from 'jsonwebtoken'
 
+export interface KakaoToken {
+  access_token: string
+  token_type: 'bearer'
+  refresh_token: string
+  id_token: string
+  expires_in: number
+  scope: string
+  refresh_token_expires_in: number
+}
+
 export async function GET(req: NextRequest) {
   // get code from user
   const code = req.nextUrl.searchParams.get('code')
