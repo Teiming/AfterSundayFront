07eb9d8d1132655db8db6e8d053c90d19f14a455
Teiming <36464297+Teiming@users.noreply.github.com>
2025-10-05 10:10:06 +0900

    feat: add btn EventGen

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 function Headline({ title }: { title: string }) {
   return <h3 style={{ padding: '0.5rem' }}>{title}</h3>
 }
@@ -7,6 +9,7 @@ export default function Page() {
     <>
       <Headline title='참가자가 많은 이벤트' />
       <Headline title='최근에 개설된 이벤트' />
+      <Link href='/EventGen'>이벤트 개설</Link>
     </>
   )
 }
