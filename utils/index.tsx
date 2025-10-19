ade3e73648231865322887a1273a065bebc6fe47
Teiming <36464297+Teiming@users.noreply.github.com>
2025-10-20 02:04:21 +0900

    fix: Call the instance only once

--- utils/index.tsx
+++ utils/index.tsx
@@ -1,8 +1,8 @@
 import { createCipheriv, randomBytes } from 'crypto'
 
 export const genCipher = (key: string, iv = randomBytes(12)) => {
-  const cipher = createCipheriv('aes-256-gcm', Buffer.from(key, 'hex'), iv)
   return (p: string) => {
+    const cipher = createCipheriv('aes-256-gcm', Buffer.from(key, 'hex'), iv)
     return {
       iv,
       cipher: Buffer.concat([cipher.update(p, 'utf8'), cipher.final()]),
