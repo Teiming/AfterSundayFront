4de92a8daf8477aaab4ed4796f517e692e356456
Teiming <36464297+Teiming@users.noreply.github.com>
2025-10-19 23:11:19 +0900

    chore: local dev

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['*.aftsunday.com'],
+  allowedDevOrigins: ['*.aftsunday.com', '192.168.0.2'],
   reactStrictMode: true,
 }
 
